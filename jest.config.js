1869c34c52d281e69da0b3a4a7517969e3627c8f
Wilfried AGO <wilfriedago@pm.me>
2023-08-30 14:16:23 +0100

    chore: update project architecture and dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-const nextJest = require('next/jest');
+const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-});
+  dir: './'
+})
 
 const customJestConfig = {
   moduleNameMapper: {
@@ -12,7 +12,7 @@ const customJestConfig = {
 
     '^@/public/(.*)$': '<rootDir>/public/$1',
 
-    '^__mocks__/(.*)$': '<rootDir>/__mocks__/$1',
+    '^__mocks__/(.*)$': '<rootDir>/__mocks__/$1'
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   clearMocks: true,
@@ -22,17 +22,17 @@ const customJestConfig = {
     '!./src/**/_*.{js,jsx,ts,tsx}',
     '!./src/**/*.stories.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
-    '!**/node_modules/**',
+    '!**/node_modules/**'
   ],
   coverageThreshold: {
     global: {
       branches: 0,
       functions: 0,
       lines: 0,
-      statements: 0,
-    },
+      statements: 0
+    }
   },
-  testEnvironment: 'jest-environment-jsdom',
-};
+  testEnvironment: 'jest-environment-jsdom'
+}
 
-module.exports = createJestConfig(customJestConfig);
+module.exports = createJestConfig(customJestConfig)
