cdb3ab854128ff7f40ec86b010bbeb3b4d079daa
Wilfried AGO <wilfriedago@pm.me>
2023-08-31 11:32:44 +0100

    chore: remove instrumentation

--- instrumentation.ts
+++
@@ -1,8 +0,0 @@
-import { registerOTel } from '@vercel/otel'
-
-// TODO : Finalize the setup by adding collector URL and service name
-// Check https://opentelemetry.io/docs/collector/getting-started/ for more details
-
-export function register() {
-  registerOTel('next-app')
-}
